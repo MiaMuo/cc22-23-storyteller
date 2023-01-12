d0d83e12f699d5ceae936c115203a15389c213fd
Mingfei Cui <mingfey.zui@gmail.com>
2023-01-12 19:31:18 +0100

    Data Structure of genre framing

--- src/main.py
+++ src/main.py
@@ -46,25 +46,37 @@ def user_prompt_n_imgs(n):
 
 
 def prompt_user_for_number_of_images():
-    num_images = input("How many images would you like to use? ")
+    num_images = input("How many images would you like to use? \n")
     return int(num_images)
 
 
 def prompt_user_for_genre():
-    # "[0] for news report \n [1] for sitcom script\n [2] for Shakespeare play?..."
 
     genre_list_with_numbers_str = "\n".join(
         [f"[{i}] for {genre['name']}" for i, genre in enumerate(gpt3.genres)])
 
-    genre = input(
+    genre_index = input(
         "What genre would you like the story to be? Enter:\n" + genre_list_with_numbers_str + "\n")
 
-    if int(genre) >= len(gpt3.genres):
+    if int(genre_index) >= len(gpt3.genres):
         print("Invalid genre. Please enter " +
               list(range(len(gpt3.genres))) + " genres. ")
         return prompt_user_for_genre()
 
-    return int(genre)
+    genre = gpt3.genres[int(genre_index)]
+
+    genreOption_list_with_numbers_str = "\n".join(
+        [f"[{i}] for {genreOption['name']}" for i, genreOption in enumerate(genre["options"])])
+
+    genre_option_index = input(
+        "What genre option would you like the story to be? Enter:\n" + genreOption_list_with_numbers_str + "\n")
+
+    if int(genre_option_index) >= len(genre["options"]):
+        print("Invalid genre option. Please enter " +
+              list(range(len(genre["options"]))) + " genres. ")
+        return prompt_user_for_genre()
+
+    return genre["options"][int(genre_option_index)]
 
 
 def prompt_user_for_mood():
@@ -93,12 +105,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# TODO
-# User specify Hyperparameter
-# Which languages do you want your story be presented, German,Chinese,Russian,Italy..?
-# Longer Story? Hayperparameter== token-length, mit selbe captions
-# different mood? Hayperparameter== mood
-# Grammar
-# Coherence
